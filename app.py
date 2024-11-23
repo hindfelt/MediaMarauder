ed0f570aa5c80a99826861a93d444f9724a52544
mathin <hindfelt@gmail.com>
2024-11-23 17:14:57 +0100

    testing pipeline to server

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     hostname = socket.gethostname()
-    return f'Hello from {hostname}! Your app is running!'
+    return f'Hello from {hostname}! This is a test for the pi'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
