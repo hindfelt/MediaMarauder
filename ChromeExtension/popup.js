d9d70fac100fe1815691168406458f0ac0f80d1c
mathin <hindfelt@gmail.com>
2024-11-26 09:19:40 +0100

    cert exception

--- ChromeExtension/popup.js
+++ ChromeExtension/popup.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         const currentUrl = tabs[0].url;
         
-        fetch('http://hardly.access.ly:8181/svtdl-hook', {
+        fetch('https://hardly.access.ly/svtdl-hook', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
